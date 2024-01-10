ed34f29b7b53da005c222441a966af5128a34436
areidakpena-ssis <153045302+areidakpena-ssis@users.noreply.github.com>
2024-01-10 13:59:56 +0700

    Add red circles
    Add in an array of red circles to cover the canvas.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
 function setup() {
   createCanvas(400, 400);
+  
+  NUM_ROWS = 10;
+  NUM_COLS = 10;
+  D = 400 / NUM_COLS; // diameter
+  
+  D = 400 / NUM_COLS;
+  
+  for (var i = 0; i < NUM_ROWS; i++) {
+    for (var j = 0; j  < NUM_COLS; j++) {
+      let xPos = j*D + (D/2);
+      let yPos = i*D + D/2;
+      
+      fill(255,80,120);
+      
+      circle(xPos, yPos, D);
+      
+    }
+  }
 }
 
 function draw() {
-  background(220);
+  
 }
